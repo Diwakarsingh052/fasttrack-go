8d8700d4b93b0b8325b6e6fa4b6464afffb793cb
“Diwakar” <diwakarsingh052@gmail.com>
2022-11-09 15:31:12 +0530

    .

--- 7-slice/slicing-prob'.go
+++ 7-slice/slicing-prob'.go
@@ -6,7 +6,7 @@ import "fmt"
 		a:= 10,20,30,40
 		b := i[2:5]
 		a ,b-> [10,20,(30,40,50),60] // backing array // a and b slice shares the same backing array. it is not a copy
-		b[0] = 999 // b is sharing the same backing array with so any changes in b will also result changes in a slice
+		b[0] = 999 // b is sharing the same backing array with a slice, so any updates in b will also result changes in a slice
 	    a ,b-> [10,20,(999,40,50),60]
 */
 func main() {
